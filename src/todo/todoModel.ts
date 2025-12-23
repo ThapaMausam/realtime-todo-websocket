8b79b000da0d210985ecc32406eecdfe9deda80d
Mausam Thapa <mausamwork@gmail.com>
2025-12-23 09:48:36 +0545

    Created DB Schema

---
+++ src/todo/todoModel.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { Status, type ITodo } from "./todoTypes.ts";
+
+const todoSchema = new mongoose.Schema<ITodo>({
+    task: {
+        type: String
+    },
+    deadline: {
+        type: String
+    },
+    status: {
+        type: String,
+        enum: [Status.Completed, Status.Pending],
+        default: Status.Pending
+    }
+})
+
+export const Todo = mongoose.model("Todo", todoSchema)
